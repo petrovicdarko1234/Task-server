195bbce487839bfa2e0046a826caec628c3c269f
Darko <none>
2023-10-08 14:13:17 +0200

    log in/create acc

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import { readFile, writeFile } from "fs/promises"
 
 const app: Application = express();
 
+//serviraju se staticki fajlovi iz ovaj folder
+//kada odem na localhost:4000 on mi izbaci index.html
+//a mogu da odem i na localhost:4000/index.html
+//ako kazem window.location.href = "tasks.html" => localhost:4000/tasks.html
+//Server radi 2 posla, jedan je kao github pages servira staticki fajlovi (html + js + css + sta god jos)
+//A drugi posao je da ima http request response
+app.use(express.static("../Tasks/")) //GITHUB PAGES
+
 app.use(cors())
 
 type Task = {
@@ -12,20 +20,81 @@ type Task = {
     Completed: boolean
 }
 
-//GET
-app.get("/api/task/:userID", async function draw(req, resp) {
-    let Path: string = ".json"
-    let tasks: Task[] = await readFromPath(req.params.userID + Path)
-    resp.send(tasks)
-})
-
-//Create
+type User = {
+    Username: string
+    Password: string
+    Id: number
+}
 app.use(express.json());
+//create User
+app.post("/api/create", async function (req, resp) {
+    console.log(req.body)
+    let n: number
+
+    let users: User[] = await readUsers("users.json")
+
+    if (users.length == 0) {
+        n = 0
+    } else {
+        n = users[users.length - 1].Id + 1
+    }
+
+
+    let newUser = {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Id: n
+    }
+
+    for (let i = 0; i < users.length; i++) {
+        if (newUser.Username == users[i].Username) {
+            resp.send({ Id: -1 })
+            return
+        }
+    }
+
+
+    users.push(newUser)
+    await writeToPath("users.json", JSON.stringify(users))
+    console.log(users)
+
+    writeToPath(n + ".json", "[]")
+
+    resp.send({ Id: newUser.Id })
+
+})
+//User
+app.post("/api/login", async function (req, resp) {
+    console.log(req.body)
+    let users: User[] = await readUsers("users.json")
+
+    for (let i = 0; i < users.length; i++) {
+        if (req.body.Username == users[i].Username && req.body.Password == users[i].Password) {
+            resp.send({ Id: users[i].Id })
+        }
+    }
+    resp.send({ Id: -1 })
+})
+//GET
+app.get("/api/task/:userID", async function (req, resp) {
+    let Path: string = ".json"
+    try {
+        let tasks: Task[] = await readFromPath(req.params.userID + Path)
+        resp.send(tasks)
+    } catch (e) {
+        resp.send({}) //todo wrong user id..
+    }
+})
+//Create
 
 app.post("/api/task/:userID", async function (req, resp) {
 
-    let Path: string = ".json"
-    let tasks: Task[] = await readFromPath(req.params.userID + Path)
+
+    let tasks: Task[] = []
+
+    let path: string = ".json"
+    tasks = await readFromPath(req.params.userID + path)
+
     let n: number
     if (tasks.length == 0) {
         n = 0
@@ -39,7 +108,7 @@ app.post("/api/task/:userID", async function (req, resp) {
         Completed: false
     }
     tasks.push(newTask)
-    await writeToPath(req.params.userID + Path, JSON.stringify(tasks))
+    await writeToPath(req.params.userID + path, JSON.stringify(tasks))
     //always have to send something
     resp.send({})
 })
@@ -127,6 +196,13 @@ async function readFromPath(path: string): Promise<Task[]> {
     //Converts a JavaScript Object Notation (JSON) string into an object.
     let tasks = JSON.parse(content) as Task[]
     return tasks
+}
+async function readUsers(path: string): Promise<User[]> {
+    const content = await readFile(path, "utf-8")
+
+
+    let user = JSON.parse(content) as User[]
+    return user
 }
 
 async function writeToPath(path: string, content: string) {
